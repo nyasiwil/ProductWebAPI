3287ba987fc8c9037b9f12a8604a82878ed71b5d
Nyasia Williams <ny.williams123@gmail.com>
2020-09-08 14:52:07 -0400

    Add project files.

---
+++ Models/AppDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductWebAPI.Models
+{
+    public class AppDbContext : DbContext
+    {
+        //AppDbContext is a representation of your db
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+
+        }
+
+        //DbSet is a representation of the table in your db
+        public DbSet<Product> products { get; set; }
+
+
+    }
+}
